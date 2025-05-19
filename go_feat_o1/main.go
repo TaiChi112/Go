113b50f3a9bafef996d0791ed9f209187602dfb9
TaiChi <anothai.0978452316@gmail.com>
2025-05-19 20:19:23 +0700

    rfc: move to be module struct

--- go_feat_o1/main.go
+++ go_feat_o1/main.go
@@ -1,123 +1,40 @@
 package main
 
-import "fmt"
-
-type Stove_Type int
-
-const (
-	Stove_Gas Stove_Type = iota
-	Stove_Electric
-	Stove_Induction
-	Stove_Infrared
+import (
+	"github.com/TaiChi112/Go/go_feat_o1/noodle_pot"
+	"github.com/TaiChi112/Go/go_feat_o1/pot"
+	"github.com/TaiChi112/Go/go_feat_o1/stove"
+	"github.com/TaiChi112/Go/go_feat_o1/streaming_pot"
 )
 
-func (s Stove_Type) to_String() string {
-	switch s {
-	case Stove_Gas:
-		return "Gas"
-	case Stove_Electric:
-		return "Electric"
-	case Stove_Induction:
-		return "Induction"
-	case Stove_Infrared:
-		return "Infrared"
-	default:
-		return "Unknown"
-	}
-}
-
-type Stove struct {
-	Type Stove_Type
-}
-
-type Pot struct {
-	price        float32
-	width        int
-	height       int
-	stove        *Stove
-	amount_stove int
-}
-
-func (p *Pot) Show_Pot_Info() {
-	if p.stove == nil {
-		fmt.Println("Pot Info: No stove attached.")
-		return
-	}
-	fmt.Printf("Price: %.2f\nWidth: %d\nHeight: %d\nStove Type: %s\n", p.price, p.width, p.height, p.stove.Type.to_String())
-}
-func (p *Pot) GetPrice() float32 {
-	return p.price
-}
-func (p *Pot) GetWidth() int {
-	return p.width
-}
-func (p *Pot) GetHeight() int {
-	return p.height
-}
-func (p *Pot) GetStove() *Stove {
-	return p.stove
-}
-func (p *Pot) GetAmountStove() int {
-	return p.amount_stove
-}
-
-type Streaming_Pot struct {
-	pot   Pot
-	layer int
-}
-
-func (sp *Streaming_Pot) GetLayer() int {
-	return sp.layer
-}
-func (sp *Streaming_Pot) GetPot() Pot {
-	return sp.pot
-}
-func (sp *Streaming_Pot) Show_Streaming_Pot_Info() {
-	fmt.Println("Streaming Pot Info:")
-	sp.pot.Show_Pot_Info()
-	fmt.Println("Layer:", sp.layer)
-}
-
-type Noodle_Pot struct {
-	pot         Pot
-	compartment int
-}
-
-func (np *Noodle_Pot) GetCompartment() int {
-	return np.compartment
-}
-func (np *Noodle_Pot) GetPot() Pot {
-	return np.pot
-}
-func (np *Noodle_Pot) Show_Noodle_Pot_Info() {
-	fmt.Println("Noodle Pot Info:")
-	np.pot.Show_Pot_Info()
-	fmt.Println("Compartment:", np.compartment)
-}
-
 func main() {
-	stove := Stove{Type: Stove_Gas}
-	streamingPot := Streaming_Pot{
-		pot: Pot{
-			price:        39.99,
-			width:        25,
-			height:       20,
-			stove:        &stove,
-			amount_stove: 1,
-		},
-		layer: 2,
+	stove := []stove.Stove{
+		{Type: stove.Stove_Gas},
+		{Type: stove.Stove_Electric},
+		{Type: stove.Stove_Induction},
+		{Type: stove.Stove_Infrared},
 	}
-	streamingPot.Show_Streaming_Pot_Info()
-	println()
-	noodlePot := Noodle_Pot{
-		pot: Pot{
-			price:        49.99,
-			width:        30,
-			height:       25,
-			stove:        &stove,
-			amount_stove: 1,
+	sp := &streaming_pot.Streaming_Pot{
+		Pot: pot.Pot{
+			Price:       100.0,
+			Width:       30,
+			Height:      20,
+			Stove:       &stove[0], // Using the first stove as an example
+			AmountStove: 1,
 		},
-		compartment: 3,
+		Layer: 3,
 	}
-	noodlePot.Show_Noodle_Pot_Info()
+	sp.Show_Streaming_Pot_Info()
+
+	np := &noodle_pot.Noodle_Pot{
+		Pot: pot.Pot{
+			Price:       80.0,
+			Width:       25,
+			Height:      15,
+			Stove:       &stove[1], // Using the second stove as an example
+			AmountStove: 1,
+		},
+		Compartment: 2,
+	}
+	np.Show_Noodle_Pot_Info()
 }
